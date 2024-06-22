46aae84973cf8ef0570cb6040ad87abaefe4e9fe
Dan1el71 <ddrmhk1@gmail.com>
2024-06-22 16:45:20 -0500

    refactor: Optimize client-side rendering in entry-client.tsx

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,12 +1,6 @@
 import './styles/index.css'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './i18n/i18n.ts'
 
-ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement,
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.hydrateRoot(document.getElementById('root') as HTMLElement, <App />)
