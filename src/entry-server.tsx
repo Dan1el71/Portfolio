eca529b0d580796aafa58bddda589d73acfed8b4
Dan1el71 <ddrmhk1@gmail.com>
2024-06-22 16:42:59 -0500

    refactor: Optimize server-side rendering in entry-server.tsx

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import App from './App'
 
 export function render() {
-  const html = ReactDOMServer.renderToString(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
+  const html = ReactDOMServer.renderToString(<App />)
   return { html }
 }
